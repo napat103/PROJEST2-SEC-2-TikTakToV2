4b99aadc98f6fdc846250f7ca87e75cc98624da7
Pupha Pomngern <87852377+Pupha2546@users.noreply.github.com>
2023-04-08 13:16:47 +0700

    Update Popup.vue

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,47 +1,43 @@
 <script setup>
-
-import { RouterLink } from "vue-router";
-
-const emits = defineEmits(['clear'])
+import { RouterLink } from "vue-router"
 
 const props = defineProps({
   score: Number,
   status: String,
-  scoreToShow: Number,
-  clearSlots: Function
+  scoreToShow: Number
 })
 </script>
 
 <template>
   <div class="popup">
   <Teleport to="body">
+
       <!-- LOSE -->
       <div class="modal align-middle justify-center items-center" v-if="score < 20">
         <RouterLink :to="{name:'AccountManagement'}" active-class="active" class="bg-black bg-opacity-50 w-full h-full text-xl font-bold align-middle justify-center items-center">
-                <div class="align-middle w-full flex justify-center h-full items-center ">
-                  <h2 class="bg-yellow-400 bg-opacity-90 border-2 border-black border-opacity-90 rounded-2xl w-1/3 text-6xl m-auto py-16 text-center font-mono font-bold align-middle justify-center items-center">
-                    YOU LOSE 🎌<br />
-                    <div class="mt-6">
-                      <span class="text-4xl text-red-500 bg-black rounded-2xl p-3">
-                        RETURN
-                      </span>
-                    </div>
-                  </h2>
-                </div>
-              </RouterLink>
+          <div class="align-middle w-full flex justify-center h-full items-center ">
+            <h2 class="headertwo">
+              YOU LOSE 🎌<br />
+              <div class="mt-6">
+                <span class="text-4xl text-red-500 bg-black rounded-2xl p-3">
+                  RETURN
+                </span>
+              </div>
+            </h2>
+          </div>
+        </RouterLink>
       </div>
 
       <!-- WIN -->
       <div class="modal align-middle justify-center items-center" v-if="status == 'YOU WIN!'">
-        <button @click="status = 'YOUR STATUS'"
-          class="bg-black bg-opacity-50 w-full h-full text-xl font-bold align-middle justify-center items-center">
+        <button @click="status = 'YOUR STATUS'" class="bg-black bg-opacity-50 w-full h-full text-xl font-bold align-middle justify-center items-center">
           <div class="align-middle justify-center">
-            <h2
-              class="bg-yellow-400 bg-opacity-90 border-2 border-black border-opacity-90 rounded-2xl w-1/3 text-6xl m-auto py-16 text-center font-mono font-bold align-middle justify-center items-center">
+            <h2 class="headertwo">
               🎰:YOU WIN🎉<br />
               <div class="mt-6">
                 <span class="text-4xl text-green-500 bg-black rounded-2xl py-3 pr-3 pl-10">
-                  +{{ scoreToShow }} 💵</span>
+                  +{{ scoreToShow }} 💵
+                </span>
               </div>
             </h2>
           </div>
@@ -79,5 +75,26 @@ body {
   width: 50%;
   height: 30%;
   border: solid black 2px;
+}
+
+.headertwo {
+  background-color: yellow;
+  --tw-bg-opacity: 0.9;
+  border-width: 2px;
+  border-color: rgb(0 0 0 / var(--tw-border-opacity));
+  --tw-border-opacity: 0.9;
+  border-radius: 1rem;
+  width: 33.33%;
+  font-size: 3.75rem;
+  line-height: 1;
+  margin: auto;
+  padding-top: 4rem;
+  padding-bottom: 4rem;
+  text-align: center;
+  font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
+  font-weight: 700;
+  vertical-align: middle;
+  justify-content: center;
+  align-items: center;
 }
 </style>
